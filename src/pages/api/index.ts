f2a52895ec5c54e1f92cb663213bf8e2045d0b22
Franco Marini <franco.marini@radiumrocket.com>
2023-07-06 10:30:14 -0300

    Health endpoint

---
+++ src/pages/api/index.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getTasks, createTask } from "@/server/task/task.controller";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  switch (req.method) {
+    case "GET":
+      return res.status(200).json({
+        message: `Server is running ✅`,
+        env: process.env.NODE_ENV,
+        error: false,
+      });
+
+    default:
+      return res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}

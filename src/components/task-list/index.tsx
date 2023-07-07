6021c99c6fcca80427ca971089f5565e36c79295
Franco Marini <franco.marini@radiumrocket.com>
2023-07-07 12:43:26 -0300

    Task list logic

--- src/components/task-list/index.tsx
+++ src/components/task-list/index.tsx
@@ -6,7 +6,9 @@ import { ITaskListProps } from "./types";
 
 export default function TaskList({ tasks }: ITaskListProps) {
   return (
-    <form className={styles.container}>
+    <form className={styles.container} onSubmit={(e) => {
+      e.preventDefault();
+    }}>
       {tasks.map((task) => <Task key={String(task._id)} name={task.name} isDone={task.isDone} />)}
       <input type='submit' value="Save" />
     </form>
--- src/components/task/index.tsx
+++ src/components/task/index.tsx
@@ -1,13 +1,24 @@
 'use client'
 
+import { useState } from "react";
+
 import styles from "./task.module.css";
 import { ITaskProps } from "./types";
 
 export default function Task({ name, isDone }: ITaskProps) {
+  const [isChecked, setIsChecked] = useState(isDone)
+
   return (
     <div className={styles.container}>
-      <input type="checkbox" defaultChecked={isDone} id={name} />
-      <label htmlFor={name} className={isDone ? styles.doneStyled : ''}>{name}</label>
+      <input
+        id={name}
+        type="checkbox"
+        checked={isChecked}
+        onChange={(e) => {
+          e.stopPropagation()
+          setIsChecked((prevValue) => !prevValue);
+        }} />
+      <label htmlFor={name} className={isChecked ? styles.doneStyled : ''}>{name}</label>
     </div>
   );
 }
